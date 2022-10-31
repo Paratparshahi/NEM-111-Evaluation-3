5a2c0a7c5d191fa55900cd10bb0c5b7c34838b53
Pratik Kumar <khoonipratik@gmail.com>
2022-10-31 14:49:05 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app=express();
+
+app.use(express.json())
+app.get('/',(req,res)=>{
+   res.send("Home Page")
+})
+
+app.listen(8004,async ()=>{
+    try{
+        await
+        console.log("Connected")
+    }catch{
+        await 
+        console.log("Not Connected");
+    }
+    console.log("Listening on port 8004")
+})
