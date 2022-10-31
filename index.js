dbff6f6a9763a1cdf7b42c8fad0ab04580624ab6
Pratik Kumar <khoonipratik@gmail.com>
2022-10-31 16:20:21 +0530

    done few

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express');
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 const app=express();
-
+const connection = require('./Config/db');
+const {UserModel} = require('./models/modelUser');
 app.use(express.json())
 app.get('/',(req,res)=>{
    res.send("Home Pa")
 })
-app.post('/signup',(req,res)=>{
+console.log(UserModel);
+app.post("/signup", async (req, res) => {
     const {email, password} = req.body;
-    console.log('email',email)
+    bcrypt.hash(password, 5, async function(err, hashed_password) {
+        if(err){
+            res.send("Something went wrong, please signup later")
+        }
+        const new_user = new UserModel({
+            email : email,
+            password : hashed_password
+        })
+        await new_user.save();
+        console.log(email,password)
+        res.send("Sign up successfull")
+    });
 })
-app.listen(8004,async ()=>{
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    const user = await UserModel.findOne({email})
+    const hashed_password = user.password
+    bcrypt.compare(password, hashed_password, function(err, result) {
+        if(result){
+            const token = jwt.sign({email : email}, 'abcd12345')
+            res.send({"msg" : "Login successfull", "token" : token})
+        }
+        else{
+            res.send("Login failed")
+        }
+    });
+    
+})
+app.listen(8005,async ()=>{
     try{
         await
         console.log("Connected")
